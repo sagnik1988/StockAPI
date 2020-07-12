469dacffc5884e33eb3b0879e44dd972f3089621
sagnik1988 <sagnik4u007@gmail.com>
2020-07-13 01:10:58 +0200

    Update Statistics.java

--- src/main/java/com/assesment/stock/dto/Statistics.java
+++ src/main/java/com/assesment/stock/dto/Statistics.java
@@ -1,52 +1,51 @@
-package com.assesment.stock.dto;
+package com.assesment.stock.model;
 
-import com.assesment.stock.model.Tick;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+
+import java.io.Serializable;
+import java.text.DecimalFormat;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Statistics {
-
-    long timestamp;
+public class GlobalStatistics implements Serializable {
     int count;
+    @JsonIgnore
     double sum;
     double avg;
     double max;
     double min;
-
-    public Statistics(Tick tick) {
-        timestamp=tick.getTimestamp();
-        count=1;
-        sum=max=min=tick.getPrice();
-    }
-
-    public void incrementCounter() {
-        count++;
-    }
 
     public void updateCounter(int counter) {
         count+=counter;
     }
 
     public  void updateSum(double stockPrice) {
-        sum+= stockPrice;
+        sum = Math.round((sum+stockPrice)*1e2)/1e2;
     }
 
     public void updateMax(double stockPrice) {
-        max = Math.max(max, stockPrice);
+        if(max>0) {
+            max = Math.max(max, stockPrice);
+        } else {
+            max = Math.max(Double.MIN_VALUE, stockPrice);
+        }
+        max = Math.round((max)*1e2)/1e2;
     }
 
     public void updateMin(double stockPrice) {
-        min = Math.min(min, stockPrice);
+        if (min>0) {
+            min = Math.min(min, stockPrice);
+        } else {
+            min = Math.min(Double.MAX_VALUE, stockPrice);
+        }
+        min = Math.round((min)*1e2)/1e2;
     }
 
-    public void updateStat(Statistics oldStat) {
-        updateCounter(oldStat.getCount());
-        updateSum(oldStat.getSum());
-        updateMax(oldStat.getMax());
-        updateMin(oldStat.getMin());
+    public void updateAvg() {
+        avg = Math.round((sum/count)*1e2)/1e2;
     }
 }
